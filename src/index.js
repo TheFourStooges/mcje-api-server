cb1eed0932f814d224ee55c74a09001c73df387e
pcminh <pcminh@users.noreply.github.com>
2021-11-04 22:25:46 +0700

    Temp workaround, use both `mongoose` and `sequelize`

--- src/index.js
+++ src/index.js
@@ -1,10 +1,36 @@
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 const sequelize = require('./config/sequelize');
 const app = require('./app');
 const config = require('./config/config');
 const logger = require('./config/logger');
 
 let server;
+
+const connectDb = async () => {
+  try {
+    await mongoose.connect(config.mongoose.url, config.mongoose.options);
+    logger.info('Connected to MongoDB');
+  } catch (error) {
+    throw Error('Error while establishing connection with MongoDB instance');
+  }
+
+  try {
+    sequelize.sync().then(() => logger.info('Sequelize models synchronized'));
+    await sequelize.authenticate();
+    logger.info('Connected to SQL Database via Sequelize ORM');
+  } catch (error) {
+    throw Error('Error while establishing connection with SQL database');
+  }
+};
+
+connectDb()
+  .then(() => {
+    server = app.listen(config.port, () => {
+      logger.info(`Listening to port ${config.port}`);
+    });
+  })
+  .catch((error) => logger.error(error));
+
 // mongoose.connect(config.mongoose.url, config.mongoose.options).then(() => {
 //   logger.info('Connected to MongoDB');
 //   server = app.listen(config.port, () => {
@@ -12,18 +38,18 @@ let server;
 //   });
 // });
 
-sequelize.sync();
-sequelize
-  .authenticate()
-  .then(() => {
-    logger.info('Connected to SQL Database using Sequelize ORM');
-    server = app.listen(config.port, () => {
-      logger.info(`Listening to port ${config.port}`);
-    });
-  })
-  .catch((error) => {
-    logger.info('Error while connecting to the database: ', error);
-  });
+// sequelize.sync();
+// sequelize
+//   .authenticate()
+//   .then(() => {
+//     logger.info('Connected to SQL Database using Sequelize ORM');
+//     server = app.listen(config.port, () => {
+//       logger.info(`Listening to port ${config.port}`);
+//     });
+//   })
+//   .catch((error) => {
+//     logger.info('Error while connecting to the database: ', error);
+//   });
 
 const exitHandler = () => {
   if (server) {
