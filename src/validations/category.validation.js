4c845789f9316ba279072fc453b5ff6132571a70
pcminh <pcminh@users.noreply.github.com>
2021-11-09 15:40:42 +0700

    `category` route and validation

---
+++ src/validations/category.validation.js
@@ -0,0 +1,56 @@
+const Joi = require('joi');
+const { objectId } = require('./custom.validation');
+
+const createCategory = {
+  body: Joi.object().keys({
+    name: Joi.string().required(),
+    description: Joi.string(),
+    isActive: Joi.boolean(),
+    assets: Joi.array().items(Joi.string().custom(objectId)).has(Joi.string().custom(objectId)).min(0).max(16).unique(),
+    parentId: Joi.string.custom(objectId),
+  }),
+};
+
+const getCategories = {
+  query: Joi.object().keys({
+    name: Joi.string(),
+    limit: Joi.number().integer(),
+    page: Joi.number().integer(),
+    parentId: Joi.string.custom(objectId),
+  }),
+};
+
+const getCategory = {
+  params: Joi.object().keys({
+    categoryId: Joi.string().custom(objectId),
+  }),
+};
+
+const updateCategory = {
+  params: Joi.object().keys({
+    categoryId: Joi.required().custom(objectId),
+  }),
+  body: Joi.object()
+    .keys({
+      name: Joi.string().required(),
+      description: Joi.string(),
+      isActive: Joi.boolean(),
+      assets: Joi.array().items(Joi.string().custom(objectId)).has(Joi.string().custom(objectId)).min(0).max(16).unique(),
+      parentId: Joi.string.custom(objectId),
+    })
+    .min(1),
+};
+
+const deleteCategory = {
+  params: Joi.object().keys({
+    categoryId: Joi.string().custom(objectId),
+  }),
+};
+
+module.exports = {
+  createCategory,
+  getCategories,
+  getCategory,
+  updateCategory,
+  deleteCategory,
+};
