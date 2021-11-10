8da4beb6691e27bb46f1f7f21e5ba274afb38270
pcminh <pcminh@users.noreply.github.com>
2021-11-10 20:11:39 +0700

    Renamed webId to uuid schema

--- src/validations/schemas/uuid.schema.js
+++ src/validations/schemas/uuid.schema.js
@@ -1,6 +1,6 @@
 const Joi = require('joi');
 const regexPatterns = require('../../config/regexPatterns');
 
-const webId = Joi.string().regex(regexPatterns.uuidv4);
+const uuid = Joi.string().regex(regexPatterns.uuidv4);
 
-module.exports = webId;
+module.exports = uuid;
