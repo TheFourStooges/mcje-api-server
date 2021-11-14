47a9a4089c5869b4f6c81270f810005285bbdeb0
pcminh <pcminh@users.noreply.github.com>
2021-11-14 17:54:12 +0700

    Removed remnant routes

--- src/routes/v1/product.route.js
+++ src/routes/v1/product.route.js
@@ -15,60 +15,60 @@ const productAttributeOptionController = require('../../controllers/productAttri
 
 const router = express.Router();
 
-// /product/attribute
-router
-  .route('/attribute')
-  .post(
-    auth('manageProducts'),
-    validate(productAttributeValidation.createProductAttribute),
-    productAttributeController.createProductAttribute
-  )
-  .get(validate(productAttributeValidation.getProductAttributes), productAttributeController.getProductAttributes);
+// // /product/attribute
+// router
+//   .route('/attribute')
+//   .post(
+//     auth('manageProducts'),
+//     validate(productAttributeValidation.createProductAttribute),
+//     productAttributeController.createProductAttribute
+//   )
+//   .get(validate(productAttributeValidation.getProductAttributes), productAttributeController.getProductAttributes);
 
-// /product/attribute/{attributeId}
-router
-  .route('/attribute/:attributeId')
-  .get(validate(productAttributeValidation.getProductAttribute), productAttributeController.getProductAttribute)
-  .patch(
-    auth('manageProducts'),
-    validate(productAttributeValidation.updateProductAttribute),
-    productAttributeController.updateProductAttribute
-  )
-  .delete(
-    auth('manageProducts'),
-    validate(productAttributeValidation.deleteProductAttribute),
-    productAttributeController.deleteProductAttribute
-  );
+// // /product/attribute/{attributeId}
+// router
+//   .route('/attribute/:attributeId')
+//   .get(validate(productAttributeValidation.getProductAttribute), productAttributeController.getProductAttribute)
+//   .patch(
+//     auth('manageProducts'),
+//     validate(productAttributeValidation.updateProductAttribute),
+//     productAttributeController.updateProductAttribute
+//   )
+//   .delete(
+//     auth('manageProducts'),
+//     validate(productAttributeValidation.deleteProductAttribute),
+//     productAttributeController.deleteProductAttribute
+//   );
 
-// /product/attribute/{attributeId}/attribute_option
-router
-  .route('/attribute/:attributeId/attribute_option')
-  .post(
-    auth('manageProducts'),
-    validate(productAttributeOptionValidation.createProductAttributeOption),
-    productAttributeOptionController.createProductAttributeOption
-  )
-  .get(
-    validate(productAttributeOptionValidation.getProductAttributeOptions),
-    productAttributeOptionController.getProductAttributeOptions
-  );
-// /product/attribute/{attributeId}/attribute_option/{attributeOptionId}
-router
-  .route('/attribute/:attributeId/attribute_option/:attributeOptionId')
-  .get(
-    validate(productAttributeOptionValidation.getProductAttributeOption),
-    productAttributeOptionController.getProductAttributeOption
-  )
-  .patch(
-    auth('manageProducts'),
-    validate(productAttributeOptionValidation.updateProductAttributeOption),
-    productAttributeOptionController.updateProductAttributeOption
-  )
-  .delete(
-    auth('manageProducts'),
-    validate(productAttributeOptionValidation.deleteProductAttributeOption),
-    productAttributeOptionController.deleteProductAttributeOption
-  );
+// // /product/attribute/{attributeId}/attribute_option
+// router
+//   .route('/attribute/:attributeId/attribute_option')
+//   .post(
+//     auth('manageProducts'),
+//     validate(productAttributeOptionValidation.createProductAttributeOption),
+//     productAttributeOptionController.createProductAttributeOption
+//   )
+//   .get(
+//     validate(productAttributeOptionValidation.getProductAttributeOptions),
+//     productAttributeOptionController.getProductAttributeOptions
+//   );
+// // /product/attribute/{attributeId}/attribute_option/{attributeOptionId}
+// router
+//   .route('/attribute/:attributeId/attribute_option/:attributeOptionId')
+//   .get(
+//     validate(productAttributeOptionValidation.getProductAttributeOption),
+//     productAttributeOptionController.getProductAttributeOption
+//   )
+//   .patch(
+//     auth('manageProducts'),
+//     validate(productAttributeOptionValidation.updateProductAttributeOption),
+//     productAttributeOptionController.updateProductAttributeOption
+//   )
+//   .delete(
+//     auth('manageProducts'),
+//     validate(productAttributeOptionValidation.deleteProductAttributeOption),
+//     productAttributeOptionController.deleteProductAttributeOption
+//   );
 
 // /product
 router
@@ -83,29 +83,29 @@ router
   .patch(auth('manageProducts'), validate(productValidation.updateProduct), productController.updateProduct)
   .delete(auth('manageProducts'), validate(productValidation.deleteProduct), productController.deleteProduct);
 
-// /product/{productId}/option_group
-router
-  .route('/:productId/option_group')
-  .post(
-    auth('manageProducts'),
-    validate(productOptionGroupValidation.createProductOptionGroup),
-    productOptionGroupController.createProductOptionGroup
-  )
-  .get(validate(productOptionGroupValidation.getProductOptionGroups), productOptionGroupController.getProductOptionGroups);
+// // /product/{productId}/option_group
+// router
+//   .route('/:productId/option_group')
+//   .post(
+//     auth('manageProducts'),
+//     validate(productOptionGroupValidation.createProductOptionGroup),
+//     productOptionGroupController.createProductOptionGroup
+//   )
+//   .get(validate(productOptionGroupValidation.getProductOptionGroups), productOptionGroupController.getProductOptionGroups);
 
-// /product/{productId}/option_group/{optionGroupId}
-router
-  .route('/:productId/option_group/:optionGroupId')
-  .get(validate(productOptionGroupValidation.getProductOptionGroup), productOptionGroupController.getProductOptionGroup)
-  .patch(
-    auth('manageProducts'),
-    validate(productOptionGroupValidation.updateProductOptionGroup),
-    productOptionGroupController.updateProductOptionGroup
-  )
-  .delete(
-    auth('manageProducts'),
-    validate(productOptionGroupValidation.deleteProductOptionGroup),
-    productOptionGroupController.deleteProductOptionGroup
-  );
+// // /product/{productId}/option_group/{optionGroupId}
+// router
+//   .route('/:productId/option_group/:optionGroupId')
+//   .get(validate(productOptionGroupValidation.getProductOptionGroup), productOptionGroupController.getProductOptionGroup)
+//   .patch(
+//     auth('manageProducts'),
+//     validate(productOptionGroupValidation.updateProductOptionGroup),
+//     productOptionGroupController.updateProductOptionGroup
+//   )
+//   .delete(
+//     auth('manageProducts'),
+//     validate(productOptionGroupValidation.deleteProductOptionGroup),
+//     productOptionGroupController.deleteProductOptionGroup
+//   );
 
 module.exports = router;
