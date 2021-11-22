698422446d0e28613c16d073cf18d8f5c39dc3f2
pcminh <pcminh@users.noreply.github.com>
2021-11-22 23:07:02 +0700

    Added catchAsync.js function explanation

--- src/utils/catchAsync.js
+++ src/utils/catchAsync.js
@@ -1,3 +1,12 @@
+/**
+ * Similar to express-async-handler
+ * Handles exception inside of async express routes and passing them
+ * to your express error handlers
+ * See: https://www.npmjs.com/package/express-async-handler
+ * See: https://stackoverflow.com/questions/56973265/what-does-express-async-handler-do
+ * @param {*} fn async handler
+ * @returns synchronous equivalent to the async function
+ */
 const catchAsync = (fn) => (req, res, next) => {
   Promise.resolve(fn(req, res, next)).catch((err) => next(err));
 };
